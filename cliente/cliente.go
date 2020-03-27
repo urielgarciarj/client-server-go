2ce22b87b906dfb2648efeb4f86c30f3d6d651f9
Uriel <urielgarciarj@outlook.com>
2020-03-26 21:13:01 -0600

    first prototype finished

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -8,9 +8,11 @@ import (
 	"time"
 )
 
+var stop uint
+
 func main() {
 	defer fmt.Println("!")
-	conn, err := net.Dial("tcp", "127.0.0.1:9999")
+	conn, err := net.Dial("tcp", "192.168.50.169:9998")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -28,13 +30,14 @@ func main() {
 			fmt.Println("connection closed")
 			return
 		}
+		//HERE WE ARE RECEIVENG THE TIME
 		data := buffer[:dataSize]
 		fmt.Println("received message: ", string(data))
 		aux := string(data[:])           //Byte to string
 		timeless, _ := strconv.Atoi(aux) //String to int
-
 		fmt.Println(timeless)
 
+		//HERE WE ARE RECEIVENG THE ID OF THE PROCESS
 		buffer2 := make([]byte, 1400)
 		dataSize, err = conn.Read(buffer2)
 		if err != nil {
@@ -45,9 +48,9 @@ func main() {
 		fmt.Println("received message: ", string(data2))
 		aux = string(data2[:])     //Byte to string
 		id, _ := strconv.Atoi(aux) //String to int
-
 		fmt.Println(id)
 
+		//START OF THE PROCESS
 		process := true
 		const ValorMaximoInt = ^uint(0)
 		//exitProcess := 0
@@ -55,10 +58,22 @@ func main() {
 			idProcess := id
 			for i := uint(timeless); i < ValorMaximoInt; i++ {
 				if process {
-					fmt.Println("process: ", idProcess, "\tCon tiempo: ", i)
+					fmt.Println("process: ", idProcess, "\tWith Time: ", i)
+					stop = i
 				}
 				time.Sleep(time.Millisecond * 500)
 			}
 		}()
+
+		//END OF THE PROCESS
+		fmt.Scanln()
+		s := strconv.Itoa(id)          //int to string
+		_, err = conn.Write([]byte(s)) //takes a string
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println("Message sent: ", s)
+		break
 	}
+
 }
--- servidor/servidor.go
+++ servidor/servidor.go
@@ -5,17 +5,22 @@ import (
 	"log"
 	"net"
 	"reflect"
+	"strconv"
 	"time"
 )
 
 var process = true
 var processid = 0
 var process2 = true
+var process3 = true
+var process4 = true
+var process5 = true
 var tipo string
 var NumberOfProcess = 0
+var conexion net.Conn
 
 func main() {
-	listener, err := net.Listen("tcp", "127.0.0.1:9999")
+	listener, err := net.Listen("tcp", ":9998")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -28,7 +33,7 @@ func main() {
 		idProcess := 1
 		for i := uint(0); i < ValorMaximoInt; i++ {
 			if process {
-				fmt.Println("process: ", idProcess, "\tCon tiempo: ", i)
+				fmt.Println("process: ", idProcess, "\tWith time: ", i)
 				timeless = i
 			}
 			if exitProcess == idProcess {
@@ -41,7 +46,46 @@ func main() {
 		idProcess := 2
 		for i := uint(0); i < ValorMaximoInt; i++ {
 			if process2 {
-				fmt.Println("process: ", idProcess, "\tCon tiempo: ", i)
+				fmt.Println("process: ", idProcess, "\tWith time: ", i)
+				timeless = i
+			}
+			if exitProcess == idProcess {
+				return
+			}
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+	go func() {
+		idProcess := 3
+		for i := uint(0); i < ValorMaximoInt; i++ {
+			if process3 {
+				fmt.Println("process: ", idProcess, "\tWith time: ", i)
+				timeless = i
+			}
+			if exitProcess == idProcess {
+				return
+			}
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+	go func() {
+		idProcess := 4
+		for i := uint(0); i < ValorMaximoInt; i++ {
+			if process4 {
+				fmt.Println("process: ", idProcess, "\tWith time: ", i)
+				timeless = i
+			}
+			if exitProcess == idProcess {
+				return
+			}
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+	go func() {
+		idProcess := 5
+		for i := uint(0); i < ValorMaximoInt; i++ {
+			if process5 {
+				fmt.Println("process: ", idProcess, "\tWith time: ", i)
 				timeless = i
 			}
 			if exitProcess == idProcess {
@@ -52,10 +96,7 @@ func main() {
 	}()
 	for {
 		conn, err := listener.Accept()
-		tipo := listener.Addr()
-
-		fmt.Println(tipo)
-		fmt.Println(reflect.TypeOf(tipo))
+		//conexion = conn
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -76,15 +117,54 @@ func listenConnection(conn net.Conn, _timeless uint) {
 			process2 = false
 			processid = 2
 		}
+		if NumberOfProcess == 3 {
+			process3 = false
+			processid = 3
+		}
+		if NumberOfProcess == 4 {
+			process4 = false
+			processid = 4
+		}
+		if NumberOfProcess == 5 {
+			process5 = false
+			processid = 5
+			NumberOfProcess = 0
+		}
+
 		buffer := make([]byte, 1400)
 		dataSize, err := conn.Read(buffer)
+		data := buffer[:dataSize]
 		if err != nil {
+
 			fmt.Println("connection closed")
 			fmt.Println(err)
 			return
 		}
-		data := buffer[:dataSize]
-		fmt.Println("received message: ", string(data))
+		fmt.Println("received message! ", string(data))
+		aux := string(data[:])      //Byte to string
+		id2, _ := strconv.Atoi(aux) //String to int
+		fmt.Println(id2)
+		fmt.Println(reflect.TypeOf(id2))
+		if id2 == 1 {
+			process = true
+			return
+		}
+		if id2 == 2 {
+			process2 = true
+			return
+		}
+		if id2 == 3 {
+			process3 = true
+			return
+		}
+		if id2 == 4 {
+			process4 = true
+			return
+		}
+		if id2 == 5 {
+			process5 = true
+			return
+		}
 
 		time := fmt.Sprint(_timeless) //Convert a uint type into a string
 		_, err = conn.Write([]byte(time))
@@ -99,5 +179,6 @@ func listenConnection(conn net.Conn, _timeless uint) {
 			log.Fatalln(err)
 		}
 		fmt.Println("Message sent: ", string(id))
+
 	}
 }
